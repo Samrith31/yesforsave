3a94bd0644dd8a79ec34c5233a647a3655143547
Samrith <119127948+Samrith31@users.noreply.github.com>
2025-04-28 20:58:00 +0530

    Update next.config.ts

--- testing-main/next.config.ts
+++ testing-main/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",           // ⚡ Important for static site generation
+  images: {
+    unoptimized: true,        // ⚡ Important so that Next.js doesn't try dynamic image optimization
+  },
 };
 
 export default nextConfig;
